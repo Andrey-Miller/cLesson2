e2888088e00f7d91433e3118d7986ea9af8458b4
sinedd <sineddam@mail.ru>
2023-01-19 14:20:41 +0500

    tasks added

---
+++ task1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+//Без использования массива
+
+Console.WriteLine("Введи трехзначное число:");
+int num = Convert.ToInt32(Console.ReadLine());
+if (Math.Abs((num / 100)) < 1) {
+    Console.WriteLine("Число не трехзначное");
+    System.Environment.Exit(0);
+}
+Console.WriteLine($"Вторая цифра в числе {num} это - {Math.Abs((num%100)/10)}");
+
+//С массивом
+
+// Console.WriteLine("Введи трехзначное число:");
+// string num = Console.ReadLine();
+// if (Math.Abs((Convert.ToInt32(num) / 100)) < 1) {
+//     Console.WriteLine("Число не трехзначное");
+//     System.Environment.Exit(0);
+// }
+// if ((Convert.ToInt32(num) < 0)) {
+//     Console.WriteLine($"Вторая цифра в числе {num} это - {num[2]}");
+// }
+// else {
+//     Console.WriteLine($"Вторая цифра в числе {num} это - {num[1]}");
+// }
---
+++ task2/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+// Без использования массива
+
+Console.WriteLine("Введи число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int bigNum = num; //чтобы сохранить изначальное число, если оно было больше 1000
+if (Math.Abs((num / 100)) < 1) {
+    Console.WriteLine($"У числа {num} нет третьей цифры");
+    System.Environment.Exit(0);
+}
+else if (Math.Abs((num / 1000)) < 1) {
+    Console.WriteLine($"Третья цифра в числе {num} это - {Math.Abs(num%10)}");
+}
+else {
+    while ((Math.Abs(num / 1000)) >=1)
+    {
+        num = num / 10;
+    }
+    Console.WriteLine($"Третья цифра в числе {bigNum} это - {Math.Abs(num%10)}");
+}
+
+//С массивом
+
+// Console.WriteLine("Введи число:");
+// string num = Console.ReadLine();
+// if (Math.Abs((Convert.ToInt32(num) / 100)) < 1) {
+//     Console.WriteLine($"У числа {num} нет третьей цифры");
+//     System.Environment.Exit(0);
+// }
+// if ((Convert.ToInt32(num) < 0)) {       
+//     Console.WriteLine($"Третья цифра в числе {num} это - {num[3]}");
+// }
+// else {
+//     Console.WriteLine($"Третья цифра в числе {num} это - {num[2]}");
+// }
